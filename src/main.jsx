8e6fb07e05c49107063fc74c56d409c92c50fb44
Thomas Wright <thomaswright1@gmail.com>
2024-07-20 11:49:39 -0500

    remove js app

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
-import App2 from "./App.res.mjs";
+import App from "./App.res.mjs";
 
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App2 />
+    <App />
   </React.StrictMode>
 );
